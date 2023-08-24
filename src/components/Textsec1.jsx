53bddf35ba33b69968f29cf4d9bca99a4bee81fb
lovkesh07 <79805511+lovkesh07@users.noreply.github.com>
2023-08-24 22:24:45 +0530

    1st commit

---
+++ src/components/Textsec1.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+
+const textsec1 = () => {
+  return (
+    <div className=" ">
+      <div className=" py-12">
+        <h1 className="text-2xl pb-4 font-bold underline underline-offset-4">
+          Introduction
+        </h1>
+
+        <p className="">
+          Many Signals in Modern Communication Systems are digital. Also, analog
+          signals are transmitted digitally. Data transmission, digital
+          transmission, or digital communications is the physical transfer of
+          data (a digital bit stream or a digitized analogue signal) over a
+          point-to-point or point-to-multipoint communication channel. Reduced
+          distortion and improvement in signal to noise ratios is the great
+          advantages of digital transmission over analog transmission. PAM, PWM
+          , PPM , DM and PCM etc., are used to transmit a signal digitally. For
+          example, these pulse modulation techniques are used in optical fibers,
+          wireless channels, computer buses, etc.
+        </p>
+      </div>
+
+      <div className="">
+        <h1 className="text-2xl pb-4 font-bold underline underline-offset-4">
+          Pulse Amplitude Modulation (PAM)
+        </h1>
+
+        <p className="pb-6">
+          It is useful and important to generate a continuous-time (CT) signal
+          from a sequence in communication systems, in which discrete data—for
+          example, digital or quantized data — is to be transmitted over a
+          channel in the form of a continuous-time signal. Unlike in the case of
+          discrete-time (DT) processing of CT signals, the resulting
+          continuous-time signal will be converted back to a discrete-time
+          signal at the receiving end.
+        </p>
+        <p className="pb-6">
+          Pulse–amplitude modulation (PAM) is a form of signal modulation where
+          the message information is encoded in the amplitude of a series of
+          signal pulses.{" "}
+        </p>
+        <p className="pb-6">
+          The basic idea in PAM for communication over a CT channel is to
+          transmit a sequence of CT pulses of some pre-specified shape p(t),
+          with the sequence of pulse amplitudes carrying the information.
+        </p>
+
+        <p className="pb-6">
+          The message signal and the sequence of amplitude-modulated rectangular
+          pulses whose amplitude is in accordance with the amplitude of the
+          message signal is termed as pulse amplitude modulation signal.{" "}
+        </p>
+        <p className="pb-6">
+          There are two operations involved in the generation of the PAM signal
+        </p>
+
+        <p className="pb-6">
+          <span className="">1.</span>Instantaneous sampling of the message
+          signal (say, m(t)) every Ts seconds, where the sampling rate, fs = 1/
+          Ts is chosen in accordance with the sampling theorem.
+        </p>
+        <p className="pb-6">
+          <span className="">2.</span>Lengthening the duration of each sample so
+          obtained to some constant value T.
+        </p>
+
+        <p className="pb-6">
+          Let s(t) denote the sequence of flat-top pulse amplitude signal
+          generated then we may express the PAM signal as{" "}
+        </p>
+
+        <span className="pb-6">s(t) = ∫_(n= -∞)^∞▒〖m(nT_s 〗). h(t - nT_s)</span>
+
+        <p className="pb-6">
+          where Ts is the sampling period m(nTs) is the sample value of m(t)
+          obtained at time t = nT_s .{" "}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default textsec1;
---
+++ src/components/Textsec2.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+
+const Textsec2 = () => {
+  return (
+    <div className="">
+      <h1 className="text-2xl pb-4 font-bold underline underline-offset-4">
+        Demodulation
+      </h1>
+      <p className="pb-6">
+        Demodulation is performed by detecting the amplitude level of the
+        carrier at every single period. The number of possible pulse amplitudes
+        in analog PAM is theoretically infinite. Digital PAM reduces the number
+        of pulse amplitudes to some power of two. For example, in 4-level PAM
+        there are 22 possible discrete pulse amplitudes; in 8-level PAM there
+        are 23 possible discrete pulse amplitudes; and in 16-level PAM there are
+        24 possible discrete pulse amplitudes.
+      </p>
+
+      <h1 className="text-2xl pb-4 font-bold underline underline-offset-4">
+        Applications
+      </h1>
+      <p className="pb-6">
+        The translation of a DT signal to a CT signal appropriate for
+        transmission, and the translation back to a DT signal at the receiver,
+        are both accomplished by devices referred to as modems
+        (modulators/demodulators). Pulse Amplitude Modulation (PAM) underlies
+        the operation of a wide variety of modems.
+      </p>
+      <p className="pb-6">
+        Power consumption is low in pulse modulation. Explanation: In pulse
+        modulation, the carrier is not transmitted continuously but in pulses
+        whose width is determined by the amplitude of the modulating signal.
+      </p>
+
+      <h1 className="text-2xl pb-4 font-bold underline underline-offset-4">
+        Effect of Noise on Pulse Amplitude Modulation
+      </h1>
+      <p className="pb-6">
+        The frequency varies according to the modulating signal or message
+        signal. Due to these variations in the signal frequency, interferences
+        will be there. So the noise will be great. For PAM, noise immunity is
+        less when compared to other modulation techniques. It is almost equal to
+        amplitude modulation.
+      </p>
+
+      {/* ------------------------------------------------- */}
+
+      <h1 className="text-2xl pb-4 font-bold underline underline-offset-4">
+        Pulse-width Modulation (PWM)
+      </h1>
+      <p className="pb-6">
+        Pulse-width modulation (PWM), or pulse-duration modulation (PDM), is a
+        method of controlling the average power delivered by an electrical
+        signal.{" "}
+      </p>
+      <p className="pb-6">
+        Fig: An example of PWM in an idealized inductor driven by a blue line
+        voltage source modulated as a series of pulses, resulting in a red line
+        sine-like current in the inductor. The rectangular voltage pulses
+        nonetheless result in a more and more smooth current waveform, as the
+        switching frequency increases. The current waveform is the integral of
+        the voltage waveform.
+      </p>
+
+      <h1 className="text-2xl pb-4 font-bold underline underline-offset-4">
+        Duty cycle
+      </h1>
+
+      <p className="pb-6">
+        A low duty cycle equates to low power because the power is off for most
+        of the time; the word duty cycle reflects the ratio of "on" time to the
+        regular interval or "period" of time. The duty cycle is measured in
+        percent, with 100% representing full on. A digital signal has a duty
+        cycle of 50% and looks like a "square" wave when it is on 50% of the
+        time and off the other 50%. A digital signal has a duty cycle of greater
+        than 50% when it spends more time in the on state than the off state. A
+        digital signal has a duty cycle of 50% when it alternates between the on and off states more often than not.
+      </p>
+
+     
+      <h1>Mathematical Formulation of pulse-width waveform</h1>
+<p> Pulse-width modulation uses a rectangular pulse wave whose pulse width is modulated resulting in the variation of the average value of the waveform. If we consider a pulse waveform f(t), with period T, low-value ymin, a high-value ymax, and a duty cycle D, the average value of the waveform is given by</p>
+
+<span>y ̅ = (1/T) . ∫_0^t▒f(t)dt</span>
+      {/* <span>`As f(t) is a pulse wave, its value is ymax for 0 < t < D.T and ymin for D.T < t <  T. The above expression then becomes</span> */}
+
+    </div>
+  );
+};
+
+export default Textsec2;
