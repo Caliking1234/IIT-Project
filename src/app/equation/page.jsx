2ebff9b7019aa1a62f3eef31bf85d0dc5535e3e3
Piyush <piyyush1219@gmail.com>
2023-08-26 09:14:25 +0530

    Equation input form added and curve plotted

---
+++ src/app/equation/page.jsx
@@ -0,0 +1,22 @@
+// import Head from 'next/head';
+import DiscreteEquationInputForm from './DiscreteEquationInputForm';
+import EquationInputForm from './EquationInputForm';
+
+export default function page() {
+  return (
+    <div>
+      {/* <Head>
+        <title>Equation Input and Plotting</title>
+        <meta
+          name="description"
+          content="Taking equations as input and plotting the corresponding curves in Next.js"
+        />
+      </Head> */}
+      <main>
+        <h1>Equation Input and Plotting</h1>
+        <EquationInputForm />
+        <DiscreteEquationInputForm/>
+      </main>
+    </div>
+  );
+}
