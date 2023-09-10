9792d84c64ec2a1157ee8f723e3c430118f5a371
Piyush <piyyush1219@gmail.com>
2023-09-10 12:49:29 +0530

    simulation1 done

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,10 @@
-import Image from "next/image";
-import Textsec1 from "../components/Textsec1";
-import Textsec2 from "@/components/Textsec2";
-import Textsec3 from "@/components/Textsec3";
-import Textsec4 from "@/components/Textsec4";
-import Textsec5 from "@/components/Textsec5";
-
-import Experiment from "@/components/experiment";
-import Sidebar from "@/components/Sidebar";
 import Landingpg from "./Landingpg";
-import PAM from "@/components/PAM";
 
 export default function Home() {
   return (
     <main>
       <div>
-        {/* <Landingpg /> */}
-        <PAM />
+        <Landingpg />
       </div>
     </main>
   );
